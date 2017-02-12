89401649c34bbe76ab77c584e9ebe59a5814eb41
Krzysztof Kondrak <krzysztof.kondrak@gmail.com>
2017-02-12 19:12:12 +0100

    indentation

--- SRC/MAIN.C
+++ SRC/MAIN.C
@@ -35,33 +35,31 @@ void draw(int x, int y, unsigned char color)
 
 void Bresenham(int x0, int y0, int x1, int y1, unsigned char color)
 {
-  int x = x1 - x0;
-  int y = y1 - y0;
-  int ax = abs(x), ay = abs(y);
-  int dx1 = x < 0 ? -1 : x > 0 ? 1 : 0;
-  int dy1 = y < 0 ? -1 : y > 0 ? 1 : 0;
-  int dx2 = dx1, dy2 = 0;
-  int n, i = 0;
+    int x = x1 - x0;
+    int y = y1 - y0;
+    int ax = abs(x), ay = abs(y);
+    int dx1 = x < 0 ? -1 : x > 0 ? 1 : 0;
+    int dy1 = y < 0 ? -1 : y > 0 ? 1 : 0;
+    int dx2 = dx1, dy2 = 0;
+    int n, i = 0;
 
-  if(ax <= ay)
-  {
-      // swap ax <=> ay
-      ax ^= ay; ay ^= ax; ax ^= ay;
-      dx2 = 0; dy2 = dy1;
-  }
+    if(ax <= ay)
+    {
+        // swap ax <=> ay
+        ax ^= ay; ay ^= ax; ax ^= ay;
+        dx2 = 0; dy2 = dy1;
+    }
 
-  n = ax >> 1;
+    n = ax >> 1;
 
-  for(; i <= ax; ++i)
-  {
-      draw(x0, y0, color);
-      n += ay;
-
-      if (n >= ax)
-      { x0 += dx1; y0 += dy1; n -= ax; }
-      else
-      { x0 += dx2; y0 += dy2;           }
-  }
+    for(; i <= ax; ++i)
+    {
+        draw(x0, y0, color);
+        n  += ay;
+        x0 += n >= ax ? dx1 : dx2;
+        y0 += n >= ax ? dy1 : dy2;
+        n  -= n >= ax ? ax  : 0;
+    }
 }
 
 unsigned short *keysPressed;
