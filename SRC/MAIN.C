ba7ff4d1766a2014ff1e68bb782b33c89af6d51e
Krzysztof Kondrak <krzysztof.kondrak@gmail.com>
2017-02-12 19:02:02 +0100

    Bresenham line drawing

--- SRC/MAIN.C
+++ SRC/MAIN.C
@@ -28,6 +28,58 @@ void Shutdown(int exitCode)
     exit(exitCode);
 }
 
+void draw(int x, int y, unsigned char color)
+{
+    VGA[(y << 8) + (y << 6) + x] = color;
+}
+
+void Bresenham(int x0, int y0, int x1, int y1, unsigned char color)
+{
+  int x = x1 - x0;
+  int y = y1 - y0;
+  int ax = abs(x), ay = abs(y);
+  int dx1 = x < 0 ? -1 : x > 0 ? 1 : 0;
+  int dy1 = y < 0 ? -1 : y > 0 ? 1 : 0;
+  int dx2 = dx1, dy2 = 0;
+  int n, i = 0;
+
+  if(ax <= ay)
+  {
+      // swap ax <=> ay
+      ax ^= ay; ay ^= ax; ax ^= ay;
+      dx2 = 0; dy2 = dy1;
+  }
+
+  n = ax >> 1;
+
+  for(; i <= ax; ++i)
+  {
+      draw(x0, y0, color);
+      n += ay;
+
+      if (n >= ax)
+      { x0 += dx1; y0 += dy1; n -= ax; }
+      else
+      { x0 += dx2; y0 += dy2;           }
+  }
+}
+
+unsigned short *keysPressed;
+
+void drawCircle(int ox, int oy, int r)
+{
+    float l = 0.0f;
+    for(l; l < 2.0f*M_PI; l += 0.001f)
+    {
+      int color = (int)((l * 10.f) + 1) % 256;
+      Bresenham(ox, oy, ox + r * cos(l), oy + r * sin(l), color);
+      draw(ox + r * cos(l), oy + r * sin(l), color);
+      keysPressed = translateInput();
+      if(keysPressed[KEY_ESC]) return;
+    }
+}
+
+
 /* ***** */
 
 int main(int argc, char **argv)
@@ -35,8 +87,9 @@ int main(int argc, char **argv)
     float invWidth, invHeight, aspectRatio, angle;
     int x, y, renderFinished = 0;
     int fov = 45;
-    unsigned short *keysPressed = translateInput();
 
+    keysPressed = translateInput();
+    
     for (x = 0; x < argc; x++)
     {
         // custom fov flag
@@ -65,17 +118,21 @@ int main(int argc, char **argv)
                 for (y = 0; y < SCREEN_HEIGHT; y++)
                 {
                     // draw the pixel
-                    VGA[(y << 8) + (y << 6) + x] = 3;
+                    VGA[(y << 8) + (y << 6) + x] = 0;
                 }
             }
+
+            drawCircle(160, 100, 95);
+            
             renderFinished = 1;
+            keysPressed = translateInput();
         }
         else
         {
             keysPressed = translateInput();
         }
     }
-
+    
     Shutdown(0);
     return 0;
 }
