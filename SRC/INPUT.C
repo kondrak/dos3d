3be27cc4f260951367fa2d3b676675c5adb95f42
Krzysztof Kondrak <krzysztof.kondrak@gmail.com>
2017-06-26 23:14:14 +0200

    updated input handler

--- SRC/INPUT.C
+++ SRC/INPUT.C
@@ -6,18 +6,25 @@ static unsigned short keys[0x81];
 // faster keyhit detector than kbhit()
 const unsigned short *kbd_getInput()
 {
-    unsigned char c, al, ah;
+    unsigned char key = '\0';
 
     // get last key 
-    c = inpd(0x60);
-    if (c >= 0x80) keys[c - 0x80] = 0;
-    if (c < 0x80)  keys[c] = 1;
-    al = ah = inpd(0x61);
-    al |= 0x80;
-    outpd(0x61, al);
-    outpd(0x61, ah);
+    _asm {
+        in al, 60h
+        mov key, al
+        in al, 61h
+        or al, 128
+        out 61h, al
+        xor al, 128
+        out 61h, al
+    }
+
+    // clear keyboard buffer
     outpd(0x20, 0x20);
     outpd(0x20, 0x20);
+
+    if (key >= 0x80) keys[key - 0x80] = 0;
+    if (key < 0x80)  keys[key] = 1;
 
     return &keys[0];
 }
